399b9eb8d93ae0ce142ceffd94560a69ef0a78ab
Amaniya Faizal <amaniyafaizal@gmail.com>
2025-07-10 23:49:12 +0530

    constants file fix

---
+++ src/constants/movieData.js
@@ -0,0 +1,28 @@
+import { siteImages } from "../assets/images";
+
+export const movieTrends = [
+    {
+        id: '1',
+        imageUrl: siteImages.MoviesTrends.SquidGame
+    }, 
+    {
+        id: '2',
+        imageUrl: siteImages.MoviesTrends.Openheimer
+    },
+    {
+        id: '3',
+        imageUrl: siteImages.MoviesTrends.Flash
+    },
+    {
+        id: '4',
+        imageUrl: siteImages.MoviesTrends.SriAsih
+    },
+    {
+        id: '5',
+        imageUrl: siteImages.MoviesTrends.DungeonsNDragons
+    },
+    {
+        id: '6',
+        imageUrl: siteImages.MoviesTrends.TheLastOfUs
+    }
+]
