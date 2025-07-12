d44d0b424b9e42a120d7288a897c356601daa47f
Amaniya Faizal <amaniyafaizal@gmail.com>
2025-07-13 01:08:51 +0530

    loader and search optimisation implemented

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import HeroSection from "../components/heroSection";
 import Trending from "../components/trending";
 import { movieTrends } from "../constants/movieData";
 import PopularSection from "../components/popularSection";
-import MovieDetailModal from "../components/movieDetailModal";
+import { siteImages } from "../assets/images";
+import { Input } from "antd";
+import { useDebounce } from "react-use";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -22,35 +25,53 @@ const Home = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [genresList, setGenresList] = useState([]);
-  const fetchMovieData = async () => {
-    setIsLoading(true);
-    setErrorMessage("");
+  const [searchMovie, setSearchMovie] = useState("");
+  const [debounceSearch, setDebounceSearch] = useState("");
 
-    try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+  useDebounce(() => setDebounceSearch(searchMovie), 500, [searchMovie]);
 
-      const response = await fetch(endpoint, API_OPTIONS);
+  const fetchMovieData = useCallback(
+    async (query = "") => {
+      setIsLoading(true);
+      setErrorMessage("");
 
-      if (!response.ok) {
-        throw new Error("Failed to fetch movies");
+      try {
+        const endpoint = query
+          ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+          : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+        const response = await fetch(endpoint, API_OPTIONS);
+        console.log(response);
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch movies");
+        }
+
+        const data = await response.json();
+
+        if (data.response === "False") {
+          setErrorMessage(data.error || "Failed to fetch movies");
+          setMovieList([]);
+          return;
+        }
+
+        setMovieList(data.results || []);
+
+        if (query && data.results.length === 0) {
+          setErrorMessage("No movies found for your search");
+        } else {
+          setErrorMessage("");
+        }
+      } catch (error) {
+        console.error("Error fetching movie data:", error);
+        setErrorMessage(error.message || "An error occurred");
+      } finally {
+        setIsLoading(false);
       }
+    },
+    []
+  );
 
-      const data = await response.json();
-
-      if (data.response === "False") {
-        setErrorMessage(data.error || "Failed to fetch movies");
-        setMovieList([]);
-        return;
-      }
-
-      setMovieList(data.results || []);
-    } catch (error) {
-      console.error("Error fetching movie data:", error);
-      setErrorMessage(error.message || "An error occurred");
-    } finally {
-      setIsLoading(false);
-    }
-  };
   const fetchGenres = async () => {
     try {
       const endpointGenres = `${API_BASE_URL}/genre/movie/list?language=en`;
@@ -71,23 +92,42 @@ const Home = () => {
   };
 
   useEffect(() => {
-    fetchMovieData();
-    fetchGenres()
-  }, []);
+    fetchGenres();
+  }, [])
+
+  useEffect(() => {
+    fetchMovieData(debounceSearch);
+  }, [debounceSearch, fetchMovieData]);
 
   return (
     <div className="home-container pb-24">
-      <div className="home-hero-section bg-cover bg-no-repeat bg-center">
+      <div className="home-hero-section bg-cover bg-no-repeat bg-center text-center">
         <HeroSection />
+
+        <Input
+          placeholder="Search through 300+ movies online"
+          prefix={
+            <img
+              src={siteImages.Icons.SearchIconPurple}
+              alt="search-icon"
+              className="h-5 mr-2.5"
+            />
+          }
+          className="hero-search-input mb-[65px] m-auto"
+          value={searchMovie}
+          onChange={(e) => setSearchMovie(e.target.value)}
+        />
       </div>
 
       <Trending trendMovies={movieTrends} />
 
       <div className="popular-container px-20">
-        <p className="title text-white mb-7">Popular</p>
+        <p className="title text-white mb-7">All Movies</p>
         <div className="flex flex-wrap justify-between gap-6 mb-8">
           {isLoading ? (
-            <div className="loading-spinner">Loading...</div>
+            <div className="loading-spinner m-auto">
+              <LoadingSpinner />
+            </div>
           ) : errorMessage ? (
             <div className="error-message">{errorMessage}</div>
           ) : (
